14cca8c698fe3b8ad0425ec851917bf5dac27f71
Marcelo Battistini <mabattistini@gmail.com>
2020-12-31 19:02:58 -0300

    Rotas de usuario

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,65 +1,107 @@
 /**
- * Arquivo: src/controllers/user.controller.js
+ * Arquivo: controllers/user.controller.js
  * Descrição: arquivo responsável pelo CRUD da classe 'Users'
  * Data: 31/12/2020
  * Author: Marcelo Battistini
  */
 
+const md5 = require('md5');
+
 const db = require('../config/database');
+
 
 // ==> Método responsável por criar um novo 'User':
 exports.createUser = async (req, res) => {
-    const { User_name, quantity, price } = req.body;
-    const response = await db.query(
-        'INSERT INTO Users (User_name, quantity, price) VALUES ($1, $2, $3)',
-        [User_name, quantity, price],
-    );
+    const {email, full_name, password, phone_number} = req.body;
+    try {
+        const response = await db.query(
+            'INSERT INTO users (email, full_name, "password", phone_number) VALUES($1, $2, $3, $4) returning id;',
+            [email, full_name, md5(password), phone_number],
+        );
+        let userId = response.rows[0].id
 
-    res.status(201).send({
-        message: 'User added successfully!',
-        body: {
-            User: { User_name, quantity, price },
-        },
-    });
+        res.status(201).send({
+            result: "success",
+            message: 'User added successfully!',
+            user_id: userId
+        });
+    } catch (e) {
+        res.status(200).send({result: "error", message: e.message})
+    }
+
 };
 
 // ==> Método responsável por listar todos os 'Users':
 exports.listAllUsers = async (req, res) => {
-    const response = await db.query(
-        'SELECT * FROM Users ORDER BY User_name ASC',
-    );
-    res.status(200).send(response.rows);
+    try {
+        const response = await db.query(
+            'SELECT * FROM Users ORDER BY full_name ASC',
+        );
+        res.status(200).send({result:"success",users: response.rows});
+    } catch (e) {
+        res.status(200).send({result: "error", message: e.message})
+    }
 };
 
 // ==> Método responsável por selecionar 'User' pelo 'Id':
 exports.findUserById = async (req, res) => {
     const UserId = parseInt(req.params.id);
-    const response = await db.query(
-        'SELECT * FROM Users WHERE Userid = $1',
-        [UserId],
-    );
-    res.status(200).send(response.rows);
+    try {
+        const response = await db.query(
+            'SELECT * FROM Users WHERE id = $1',
+            [UserId],
+        );
+        if (response.rows.length > 0)
+            res.status(200).send({result: "success", users: response.rows[0]});
+        else res.status(200).send({result: "error", message: "Registro não encontrado"})
+    } catch (e) {
+        res.status(200).send({result: "error", message: e.message})
+    }
 };
 
 // ==> Método responsável por atualizar um 'User' pelo 'Id':
 exports.updateUserById = async (req, res) => {
     const UserId = parseInt(req.params.id);
-    const { User_name, quantity, price } = req.body;
+    const {email, full_name, phone_number} = req.body;
+    try {
+        const response = await db.query(
+            'UPDATE Users SET email = $1, full_name = $2, phone_number = $3 WHERE id = $4',
+            [email, full_name, phone_number, UserId],
+        );
 
-    const response = await db.query(
-        'UPDATE Users SET User_name = $1, quantity = $2, price = $3 WHERE UserId = $4',
-        [User_name, quantity, price, UserId],
-    );
-
-    res.status(200).send({ message: 'User Updated Successfully!' });
+        res.status(200).send({message: 'User Updated Successfully!'});
+    } catch (e) {
+        res.status(200).send({result: "error", message: e.message})
+    }
 };
 
 // ==> Método responsável por excluir um 'User' pelo 'Id':
 exports.deleteUserById = async (req, res) => {
     const UserId = parseInt(req.params.id);
-    await db.query('DELETE FROM Users WHERE UserId = $1', [
-        UserId,
-    ]);
+    try {
+        await db.query('DELETE FROM Users WHERE id = $1', [
+            UserId,
+        ]);
 
-    res.status(200).send({ message: 'User deleted successfully!', UserId });
+        res.status(200).send({message: 'User deleted successfully!', UserId});
+    } catch (e) {
+        res.status(200).send({result: "error", message: e.message})
+    }
 };
+
+// ==> Método responsável por validar o login de um 'User':
+exports.login = async (req, res) => {
+    const username = req.body.email
+    const password = md5(req.body.password)
+
+    const response = await db.query('SELECT * FROM USERS WHERE EMAIL = $1', [username])
+    let user;
+    if (response.rows.length === 0) {
+        res.status(200).send({result: "error", message: "Email ou senha inválido"})
+    } else {
+        user = response.rows[0]
+        if (user.accout_confirmed === 0)
+            res.status(200).send({result: "error", message: "Você não confirmou o seu cadastro"})
+        else  res.status(200).send({result: "success", message: "Login efetuado com sucesso", user: response.rows[0]})
+    };
+}
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * Arquivo: routes/user.js
+ * Descrição: arquivo responsável pelas rotas da classe 'Users'
+ * Data: 31/12/2020
+ * Author: Marcelo Battistini
+ */
+
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/user.controller')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/login',(req,res,next) => {
-  res.send('get login')
-})
+router.post('/create',userController.createUser )
+router.post('/login',userController.login )
+router.get('/all',userController.listAllUsers )
+router.get('/:id',userController.findUserById )
+router.put('/:id',userController.updateUserById )
+router.delete('/:id',userController.deleteUserById )
 
 module.exports = router;
